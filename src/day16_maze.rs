51e84e320363ea672fffaff4adee78e25e251387
mu suwi <cyberpunk.pizza@gmail.com>
2024-12-18 18:28:54 -0800

    day 16 part 1 (comments)

--- src/day16_maze.rs
+++ src/day16_maze.rs
@@ -27,6 +27,7 @@ fn get_tile(world: &[Vec<char>], coords: Vec2) -> char {
 pub fn main(input: &str) {
     let maze: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
+    // find 'S' in the grid
     let start: Vec2 = maze
         .iter()
         .enumerate()
@@ -41,6 +42,7 @@ pub fn main(input: &str) {
         })
         .unwrap();
 
+    // all the mutable state in one place, thx
     let mut rats: Vec<Rat> = Vec::new();
     let mut visited: HashSet<(Vec2, Vec2)> = HashSet::new();
     let mut score_submissions: HashSet<usize> = HashSet::new();
@@ -58,37 +60,45 @@ pub fn main(input: &str) {
         for (i, rat) in rats.clone().into_iter().enumerate().rev() {
             match get_tile(&maze, rat.pos) {
                 'E' => {
+                    // we reached the goal!
                     score_submissions.insert(rat.score);
                     rats.swap_remove(i);
                     continue;
                 }
                 '#' => {
+                    // reached the end of a hallway
                     rats.swap_remove(i);
                     continue;
                 }
                 _ if visited.contains(&(rat.pos, rat.dir)) => {
+                    // another rat got here first
                     rats.swap_remove(i);
                     continue;
                 }
                 _ if rat.score > 100_000 => {
+                    // you must want to go home...
                     rats.swap_remove(i);
                     continue;
                 }
                 _ => {}
             }
 
+            // after turning a corner, you'll sleep for 1000 ticks
+            // so that rats with lower scores stay in the lead.
             if rat.sleep_countdown > 0 {
                 rats[i].sleep_countdown -= 1;
                 rats[i].score += 1;
                 continue;
             }
 
+            // every time the path branches left or right, spawn a new rat facing that direction
+
             if get_tile(&maze, rat.pos + rat.dir.rotate_ccw()) == '.' {
                 let new_rat = Rat {
                     pos: rat.pos + rat.dir.rotate_ccw(),
                     dir: rat.dir.rotate_ccw(),
                     score: rat.score,
-                    sleep_countdown: 1001,
+                    sleep_countdown: 1001, // 1 more to acct for newborn rat being 1 step ahead
                 };
                 rats.push(new_rat);
                 visited.insert((rat.pos, rat.dir));
@@ -99,12 +109,13 @@ pub fn main(input: &str) {
                     pos: rat.pos + rat.dir.rotate_cw(),
                     dir: rat.dir.rotate_cw(),
                     score: rat.score,
-                    sleep_countdown: 1001,
+                    sleep_countdown: 1001, // 1 more to acct for newborn rat being 1 step ahead
                 };
                 rats.push(new_rat);
                 visited.insert((rat.pos, rat.dir));
             }
 
+            // after all that, step forward as normal
             rats[i].pos = rat.pos + rat.dir;
             rats[i].score += 1;
         }
