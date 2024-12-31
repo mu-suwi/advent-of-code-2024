873c4b2716b6a55253645af207f88fbb784e2cc5
mu suwi <cyberpunk.pizza@gmail.com>
2024-12-31 03:56:06 -0800

    let's go home ~~~~

--- src/day25_keylocks.rs
+++ src/day25_keylocks.rs
@@ -11,16 +11,15 @@ pub fn main(input: &str) {
 
     lua.globals().set("input", input).unwrap();
 
-    lua.load("local locks = {} local keys = {} input = string.gsub(input,
-      \"\\n\\n\", \"O\") for grid in string.gmatch(input, \"([^O]+)\") do grid =
-      string.gsub(grid, \"\\n\", \"\") if string.sub(grid, 1, 5) == \"#####\" 
-      then table.insert(locks, grid) else table.insert(keys, grid) end end local
-      function fits(k, l) if k == nil or l == nil then return false end for
-      i=1,#l do if string.sub(k, i, i) == \"#\" and string.sub(l, i, i) ==
-      \"#\" then return false end end return true end local matches = 0 for
-      i=1,#locks do local lock = locks[i] for j=1,#keys do local key = keys[j]
-      if fits(key, lock) then matches = matches + 1 end end end
-      print(\"matches: \"..matches)",)
+    lua.load(r##"local locks = {} local keys = {} input = string.gsub(input,
+      "\n\n", "O") for grid in string.gmatch(input, "([^O]+)") do grid = string
+      .gsub(grid, "\n", "") if string.sub(grid, 1, 5) == "....." then table
+      .insert(keys, grid) else table.insert(locks, grid) end end local function 
+      fits(k, l) if k == nil or l == nil then return false end for i=1,#l do if 
+      string.sub(k, i, i) == "#" and string.sub(l, i, i) == "#" then return 
+      false end end return true end local matches = 0 for i=1,#locks do local 
+      lock = locks[i] for j=1,#keys do local key = keys[j] if fits(key, lock) 
+      then matches = matches + 1 end end end print("matches: "..matches)"##)
     .exec()
     .unwrap();
 
